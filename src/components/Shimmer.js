be79ba278945c666d85a1ebbd7b7856380edaaf8
Vikas Pandey <100843095+alphacalling@users.noreply.github.com>
2023-11-21 15:25:28 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -5,16 +5,16 @@ export const Shimmer = () => {
         <div className="flex flex-wrap">
             {Array(50).fill("").map((e, index) => (
                 <div
-                    className="w-[17.5rem] h-[15rem] bg-slate-300 rounded-lg m-2"
+                    className="sm:w-[17.5rem] h-[15rem] bg-slate-300 rounded-lg m-2"
                     key={index}
                 >
                     <div className="flex">
                         <div className="h-[4rem] w-[4.5rem] rounded-full bg-slate-50 mt-16 ml-4"></div>
                         <div className="mt-16">
-                            <div className="w-[9rem] h-4 rounded-xl bg-slate-50 mt-5 ml-5"></div>
-                            <div className="w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-3"></div>
-                            <div className="w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-6"></div>
-                            <div className="w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-2"></div>
+                            <div className="sm:w-[9rem] h-4 rounded-xl bg-slate-50 mt-5 ml-5"></div>
+                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-3"></div>
+                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-6"></div>
+                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-2"></div>
                             {/* <div className="w-[13rem] h-4 rounded-xl bg-slate-50 mt-3 mr-2"></div> */}
                         </div>
                     </div>
@@ -29,7 +29,7 @@ export const WatchPageShimmer = () => {
         <div className="flex flex-wrap">
             {Array(1).fill("").map((e, index) => (
                 <div
-                    className="w-[26rem] h-[31rem] bg-slate-300 rounded-lg m-2"
+                    className="sm:w-[26rem] h-[31rem] bg-slate-300 rounded-lg m-2"
                     key={index}
                 ></div>
             ))}
@@ -42,14 +42,14 @@ export const CommentShimmer = () => {
         <div className="flex flex-col flex-wrap">
             {Array(3).fill("").map((e, index) => (
                 <div
-                    className="w-[30rem] h-[7.5rem] bg-slate-300 rounded-xl m-4 ml-[9rem]"
+                    className="sm:w-[30rem] h-[7.5rem] bg-slate-300 rounded-xl m-4 ml-[9rem]"
                     key={index}
                 >
                     <div className="flex">
                         <div className="h-10 w-10 rounded-2xl bg-slate-50 mt-8 m-4"></div>
-                        <div className="w-[22rem] h-4 rounded-xl bg-slate-50 mt-7">
-                            <div className="w-[22rem] h-4 rounded-xl bg-slate-50 mt-7"></div>
-                            <div className="w-[22rem] h-4 rounded-xl bg-slate-50 mt-3"></div>
+                        <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-7">
+                            <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-7"></div>
+                            <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-3"></div>
                         </div>
                     </div>
                 </div>
@@ -63,16 +63,16 @@ export const VideoInfoShimmer = () => {
         <div className="flex flex-col flex-wrap">
             {Array(1).fill("").map((e, index) => (
                 <div
-                    className="w-[50rem] h-[15rem] bg-slate-300 rounded-xl m-4 ml-[4rem]"
+                    className="sm:w-[50rem] h-[15rem] bg-slate-300 rounded-xl m-4 ml-[4rem]"
                     key={index}
                 >
                     <div className="flex">
                         <div className="w-[30rem] h-5 rounded-xl bg-slate-50 mt-7 ml-7">
-                            <div className="w-[40rem] h-5 rounded-xl bg-slate-50 mt-7 ml-4"></div>
-                            <div className="w-[35rem] h-5 rounded-xl bg-slate-50 mt-3 ml-8"></div>
-                            <div className="w-[43rem] h-5 rounded-xl bg-slate-50 mt-4 ml-3"></div>
-                            <div className="w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-7"></div>
-                            <div className="w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-4"></div>
+                            <div className="sm:w-[40rem] h-5 rounded-xl bg-slate-50 mt-7 ml-4"></div>
+                            <div className="sm:w-[35rem] h-5 rounded-xl bg-slate-50 mt-3 ml-8"></div>
+                            <div className="sm:w-[43rem] h-5 rounded-xl bg-slate-50 mt-4 ml-3"></div>
+                            <div className="sm:w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-7"></div>
+                            <div className="sm:w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-4"></div>
                         </div>
                     </div>
                 </div>
