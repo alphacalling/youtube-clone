90acfab9ad6e083a0edfd2f87a8cc4bf2027b822
Vikas Pandey <100843095+alphacalling@users.noreply.github.com>
2024-10-18 02:25:44 +0530

    css update

--- src/components/LiveChatCard.js
+++ src/components/LiveChatCard.js
@@ -1,69 +1,57 @@
 import React, { useEffect, useState } from 'react';
 import LiveChat from './LiveChat';
 import { addMessage } from '../utils/chatSlice';
-import { useDispatch, useSelector } from 'react-redux'
-import { generateRandomName, makeString } from '../utils/RandomName'
+import { useDispatch, useSelector } from 'react-redux';
+import { generateRandomName, makeString } from '../utils/RandomName';
 
 const LiveChatCard = () => {
-
     const [liveChat, setLiveChat] = useState("");
     const dispatch = useDispatch();
     const chatMessage = useSelector((store) => store.chat.messages);
-    useEffect(() => {
-        // api polling
-        const data = setInterval(() => {
 
-            //dispatch an action
+    useEffect(() => {
+        const data = setInterval(() => {
             dispatch(addMessage({
                 name: generateRandomName(),
                 message: makeString(15) + '🚀',
-            }))
+            }));
         }, 1000);
 
-        return () => {
-            clearInterval(data);
-        };
-    }, []);
+        return () => clearInterval(data);
+    }, [dispatch]);
+
     return (
-        <>
-            <div className='h-[30rem] w-96 border border-black mt-1 sm:mr-4 sm:pr-4 rounded-lg shadow'>
-                <span className='font-serif text-lg font-semibold border-b-2'>Live Chat</span>
-                <div className='h-[25rem] w-96 border
-        border-black flex flex-col-reverse overflow-y-scroll'>
-                    <div className=''>
-                        {chatMessage.map((chat, index) =>
-                            <LiveChat key={index} name={chat.name} message={chat.message}
-                            />)
-                        }
-                    </div>
+        <div className='h-[30rem] w-full max-w-lg border border-black mt-1 sm:mr-4 sm:pr-4 rounded-lg shadow-md mx-auto'>
+            <span className='font-serif text-lg font-semibold border-b-2 block text-center py-2'>Live Chat</span>
+            <div className='h-[25rem] sm:h-[20rem] md:h-[22rem] lg:h-[25rem] w-full border border-black flex flex-col-reverse overflow-y-scroll'>
+                <div>
+                    {chatMessage.map((chat, index) => (
+                        <LiveChat key={index} name={chat.name} message={chat.message} />
+                    ))}
                 </div>
-
-                <form className='focus:bg-slate-200 flex'
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        console.log("prevent default", liveChat);
-
-                        //for showing on UI (dispatch action)
-                        dispatch(addMessage({
-                            name: "Vikas",
-                            message: liveChat,
-                        }));
-                        setLiveChat("")
-                    }}>
-
-                    <input className='w-full m-1 rounded-md border border-blue-900'
-                        type="text"
-                        value={liveChat}
-                        onChange={(e) => {
-                            setLiveChat(e.target.value)
-                        }}
-                    />
-                    <button className='p-2 m-1 bg-green-300 rounded-lg'>Chat</button>
-                </form>
             </div>
-
-        </>
-    )
-}
+            <form
+                className='flex items-center p-2'
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    dispatch(addMessage({
+                        name: "Vikas",
+                        message: liveChat,
+                    }));
+                    setLiveChat("");
+                }}
+            >
+                <input
+                    className='w-full m-1 rounded-md border border-blue-900 p-2'
+                    type="text"
+                    value={liveChat}
+                    onChange={(e) => setLiveChat(e.target.value)}
+                    placeholder="Type your message..."
+                />
+                <button className='p-2 m-1 bg-green-300 rounded-lg'>Chat</button>
+            </form>
+        </div>
+    );
+};
 
 export default LiveChatCard;
