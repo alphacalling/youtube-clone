f6174f8fce176688c03a6374922b61b61037b288
Vikas Pandey <100843095+alphacalling@users.noreply.github.com>
2024-10-18 02:29:11 +0530

    css update

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,20 +2,19 @@ import React from "react";
 
 export const Shimmer = () => {
     return (
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap justify-center">
             {Array(50).fill("").map((e, index) => (
                 <div
-                    className="sm:w-[17.5rem] h-[15rem] bg-slate-300 rounded-lg m-2"
+                    className="w-full sm:w-[14.5rem] h-[12rem] bg-slate-300 rounded-lg m-2"
                     key={index}
                 >
-                    <div className="flex">
-                        <div className="h-[4rem] w-[4.5rem] rounded-full bg-slate-50 mt-16 ml-4"></div>
-                        <div className="mt-16">
-                            <div className="sm:w-[9rem] h-4 rounded-xl bg-slate-50 mt-5 ml-5"></div>
-                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-3"></div>
-                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-6"></div>
-                            <div className="sm:w-[10rem] h-4 rounded-xl bg-slate-50 mt-3 ml-2"></div>
-                            {/* <div className="w-[13rem] h-4 rounded-xl bg-slate-50 mt-3 mr-2"></div> */}
+                    <div className="flex flex-col sm:flex-row items-center sm:items-start">
+                        <div className="h-[4rem] w-[4.5rem] rounded-full bg-slate-50 mt-4 sm:mt-12 sm:ml-2"></div>
+                        <div className="flex flex-col mt-4 sm:mt-12">
+                            <div className="w-3/4 sm:w-[8rem] h-4 rounded-xl bg-slate-50 mt-2 sm:mt-3 sm:ml-3"></div>
+                            <div className="w-4/5 sm:w-[8rem] h-4 rounded-xl bg-slate-50 mt-2 sm:mt-2 sm:ml-2"></div>
+                            <div className="w-full sm:w-[8rem] h-4 rounded-xl bg-slate-50 mt-2 sm:mt-2 sm:ml-4"></div>
+                            <div className="w-3/4 sm:w-[8rem] h-4 rounded-xl bg-slate-50 mt-2 sm:mt-2 sm:ml-2"></div>
                         </div>
                     </div>
                 </div>
@@ -26,10 +25,10 @@ export const Shimmer = () => {
 
 export const WatchPageShimmer = () => {
     return (
-        <div className="flex flex-wrap">
+        <div className="flex justify-center">
             {Array(1).fill("").map((e, index) => (
                 <div
-                    className="sm:w-[26rem] h-[31rem] bg-slate-300 rounded-lg m-2"
+                    className="w-full sm:w-[26rem] h-[31rem] bg-slate-300 rounded-lg m-2"
                     key={index}
                 ></div>
             ))}
@@ -39,17 +38,17 @@ export const WatchPageShimmer = () => {
 
 export const CommentShimmer = () => {
     return (
-        <div className="flex flex-col flex-wrap">
+        <div className="flex flex-col items-center">
             {Array(3).fill("").map((e, index) => (
                 <div
-                    className="sm:w-[30rem] h-[7.5rem] bg-slate-300 rounded-xl m-4 ml-[9rem]"
+                    className="w-full sm:w-[30rem] h-[7.5rem] bg-slate-300 rounded-xl m-4 sm:ml-4"
                     key={index}
                 >
-                    <div className="flex">
-                        <div className="h-10 w-10 rounded-2xl bg-slate-50 mt-8 m-4"></div>
-                        <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-7">
-                            <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-7"></div>
-                            <div className="sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-3"></div>
+                    <div className="flex items-center">
+                        <div className="h-10 w-10 rounded-2xl bg-slate-50 mt-4 mx-4"></div>
+                        <div className="flex flex-col mt-4">
+                            <div className="w-3/4 sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-1"></div>
+                            <div className="w-3/4 sm:w-[22rem] h-4 rounded-xl bg-slate-50 mt-3"></div>
                         </div>
                     </div>
                 </div>
@@ -60,19 +59,18 @@ export const CommentShimmer = () => {
 
 export const VideoInfoShimmer = () => {
     return (
-        <div className="flex flex-col flex-wrap">
+        <div className="flex flex-col items-center">
             {Array(1).fill("").map((e, index) => (
                 <div
-                    className="sm:w-[50rem] h-[15rem] bg-slate-300 rounded-xl m-4 ml-[4rem]"
+                    className="w-full sm:w-[50rem] h-[15rem] bg-slate-300 rounded-xl m-4"
                     key={index}
                 >
-                    <div className="flex">
-                        <div className="w-[30rem] h-5 rounded-xl bg-slate-50 mt-7 ml-7">
-                            <div className="sm:w-[40rem] h-5 rounded-xl bg-slate-50 mt-7 ml-4"></div>
-                            <div className="sm:w-[35rem] h-5 rounded-xl bg-slate-50 mt-3 ml-8"></div>
-                            <div className="sm:w-[43rem] h-5 rounded-xl bg-slate-50 mt-4 ml-3"></div>
-                            <div className="sm:w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-7"></div>
-                            <div className="sm:w-[38rem] h-5 rounded-xl bg-slate-50 mt-3 ml-4"></div>
+                    <div className="flex flex-col sm:flex-row items-center sm:items-start">
+                        <div className="w-3/4 sm:w-[30rem] h-5 rounded-xl bg-slate-50 mt-4 mx-4">
+                            <div className="w-full sm:w-[40rem] h-5 rounded-xl bg-slate-50 mt-3"></div>
+                            <div className="w-full sm:w-[35rem] h-5 rounded-xl bg-slate-50 mt-3"></div>
+                            <div className="w-full sm:w-[43rem] h-5 rounded-xl bg-slate-50 mt-3"></div>
+                            <div className="w-full sm:w-[38rem] h-5 rounded-xl bg-slate-50 mt-3"></div>
                         </div>
                     </div>
                 </div>
@@ -80,4 +78,3 @@ export const VideoInfoShimmer = () => {
         </div>
     );
 };
-
